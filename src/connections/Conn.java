d5a37308f9b8d33055356465996b38e40ed3e9de
NCUNHA <>
2020-12-06 19:24:25 +0000

    connections

--- src/connections/Conn.java
+++ src/connections/Conn.java
@@ -1,9 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package connections;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
@@ -11,4 +13,59 @@ package connections;
  */
 public class Conn {
     
+        //Criação do objecto
+          
+    
+        //URL da base de dados
+        String url = "jdbc:mysql://localhost:3306/president?zeroDateTimeBehavior=CONVERT_TO_NULL";
+        
+        //utilizador e password
+        String username = "root";
+        String password = "123456";
+                
+        //Atributos
+
+        
+    //Método Construtor
+    public void conn (){        
+                 
+        try{       
+            
+             // criação do objecto para a ligaçaõ a base de dados
+            Connection connection = DriverManager.getConnection(url,username,password);
+                       
+            //Criação de objecto com base na biblioteca java.sql.Statement
+            Statement stmt = connection.createStatement();  
+                        
+            //Criação de objecto pa execução de query's com base na biblioteca java.sql.ResultSet
+            ResultSet rs = stmt.executeQuery("SELECT * from ");
+                        
+            //Percorre a base de dados a procura da informação e retorna a mesma
+            while (rs.next()) {
+                /*
+                nome = rs.getString("presidentName");
+                partido = rs.getString("politicalParty");
+                iMandato = rs.getString("startDate");
+                fMandato = rs.getString("endDate");
+                bio = rs.getString("sNarrative");
+                numero = rs.getString("presidentID");
+                votos = rs.getString("lectoralCollegeVotes");
+                imagem = rs.getString("url");
+                */
+            }      
+            
+            System.out.println("Ligado a Base de Dados"); 
+        }
+        
+        //Caso exista algum erro, e lançada uma mensagem de excepção
+        catch (SQLException e){
+            
+            System.out.println("Não foi possível ligar á Base de Dados");
+            
+            e.printStackTrace();
+            
+        }
+        
+    }        
+        
 }
