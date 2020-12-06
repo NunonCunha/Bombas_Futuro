ab64a2ec039373871a870dd6f169b24157ed04e8
NCUNHA <>
2020-12-06 21:32:37 +0000

    V2_commit

--- src/GUI/Client.java
+++ src/GUI/Client.java
@@ -28,6 +28,7 @@ public class Client extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Main");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
